87d1b3d65cd47098e6a21163a8ce00a425bb0fe6
Aleksandr Matrosov <aleksandr.matrosov@7-tv.de>
2021-04-15 11:39:00 +0200

    Optional interface properties

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log(`la la land making of`);
+import { User } from './models/User';
+
+const user = new User({ name: "my name", age: 20 });
+
+user.set({name:'new name'});
+
+console.log(`user.name = ${user.get('name')}`);
+console.log(`user.age = ${user.get('age')}`);
