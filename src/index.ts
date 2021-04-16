2b4ba0c7c8ec37acdd24b93b92cbceae9579b991
Aleksandr Matrosov <aleksandr.matrosov@7-tv.de>
2021-04-16 10:28:01 +0200

    Extracting sync and attributes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import { User } from './models/User';
 
 const user = new User({ name:'new record',age:0 });
-user.save();
+
+user.events.on('change', ()=>{
+    console.log(`change!`);
+})
+
+user.events.trigger('change')
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,39 +1,17 @@
-// @ts-ignore
-import axios, { AxiosResponse } from 'axios';
+import { Eventing } from './Eventing';
+import { Sync } from './Sync';
 
-interface UserProps {
+export interface UserProps {
     id?: number;
     name?: string;
     age?: number;
 }
 
+const rootUrl = 'http://localhost:3000/users';
+
 export class User {
-    constructor (private data: UserProps) {
-    }
+    events = new Eventing();
+    sync = new Sync<UserProps>(rootUrl);
 
-    get (propName: string): number | string {
-        // @ts-ignore
-        return this.data[propName];
-    }
 
-    set (update: UserProps): void {
-        Object.assign(this.data, update);
-    }
-
-    fetch (): void {
-        axios.get(`http://localhost:3000/users/${this.get('id')}`).
-            then((response: AxiosResponse): void => {
-                this.set(response.data);
-            })
-    }
-
-    save (): void {
-        let id = this.get('id');
-
-        if (id) {
-            axios.put(`http://localhost:3000/users/${id}`, this.data)
-        } else {
-            axios.post(`http://localhost:3000/users/`, this.data)
-        }
-    }
 }
