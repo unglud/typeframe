d933cd0cd4dc48400cfc67bb4b0837dca99765a9
Aleksandr Matrosov <aleksandr.matrosov@7-tv.de>
2021-04-16 11:29:05 +0200

    Add generic constraints

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -2,8 +2,8 @@ export class Attributes<T> {
     constructor (private data: T) {
     }
 
-    get (propName: string): number | string {
-        return this.data[propName];
+    get<K extends keyof T> (key: K): T[K] {
+        return this.data[key];
     }
 
     set (update: T): void {
