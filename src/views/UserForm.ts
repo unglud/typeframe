c96dd392236511f2ed0b358936e3abab2f019c6c
Aleksandr Matrosov <aleksandr.matrosov@7-tv.de>
2021-04-16 15:19:06 +0200

    events map bindin

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -13,15 +13,34 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       "click:button": this.onButtonClick,
+      "mouseenter:h1":this.onHeaderHover
     };
   }
 
-  onButtonClick(): void {}
+  onButtonClick(): void {
+    console.log(`Hello`);
+  }
+
+  onHeaderHover():void {
+    console.log(`hovere`);
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(":");
+
+      fragment.querySelectorAll(selector).forEach(element =>{
+        element.addEventListener(eventName,eventsMap[eventKey])
+      })
+    }
+  }
 
   render(): void {
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
 
+    this.bindEvents(templateElement.content)
     this.parent.append(templateElement.content);
   }
 }
