c003a4561906553ceec43fef87a4f63805a674a8
Aleksandr Matrosov <aleksandr.matrosov@7-tv.de>
2021-04-16 12:45:29 +0200

    Fix context

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { User } from './models/User';
 
 const user = new User({ name:'new record',age:0 });
 
-user.on('change',()=>{
-    console.log(`bird caller tongue`);
+console.log(user.get('name'));
+
+user.on('change', ()=>{
+    console.log('changed')
 })
+
+user.trigger('change');
